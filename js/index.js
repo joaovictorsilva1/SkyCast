feccc53a4cb0dc7b147db1738aa506fcf70ca12c
João Silva <joaosilvavic2017@gmail.com>
2024-03-19 21:30:48 -0300

    up1

--- js/index.js
+++ js/index.js
@@ -1,17 +1,104 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const chaveAPI = 'b55ea4a1a3f74a0893c234328241703';
-  const cidade = 'Sao Paulo'; // Substitua pela cidade desejada
+const apiKey = "af677f5f1b77351c66146ce009bf5fe5";
+const apiCountryURL = "https://flagcdn.com/16x12/br.png";
+const apiUnsplash = "https://source.unsplash.com/1600x900/?";
 
-  fetch(`http://api.weatherapi.com/v1/current.json?key=${chaveAPI}&q=${cidade}&lang=pt`)
-    .then(response => response.json())
-    .then(data => {
-      // Atualiza os elementos HTML com os dados meteorológicos
-      document.getElementById('localizacao').textContent = data.location.name;
-      document.getElementById('temperatura').textContent = data.current.temp_c;
-      document.getElementById('condicao').textContent = data.current.condition.text;
-      document.getElementById('velocidade-vento').textContent = data.current.wind_kph;
-    })
-    .catch(error => {
-      console.error('Erro ao obter os dados meteorológicos:', error);
-    });
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
+
+const weatherContainer = document.querySelector("#weather-data");
+
+const errorMessageContainer = document.querySelector("#error-message");
+const loader = document.querySelector("#loader");
+
+const suggestionContainer = document.querySelector("#suggestions");
+const suggestionButtons = document.querySelectorAll("#suggestions button");
+
+// Loader
+const toggleLoader = () => {
+  loader.classList.toggle("hide");
+};
+
+const getWeatherData = async (city) => {
+  toggleLoader();
+
+  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+  const res = await fetch(apiWeatherURL);
+  const data = await res.json();
+
+  toggleLoader();
+
+  return data;
+};
+
+// Tratamento de erro
+const showErrorMessage = () => {
+  errorMessageContainer.classList.remove("hide");
+};
+
+const hideInformation = () => {
+  errorMessageContainer.classList.add("hide");
+  weatherContainer.classList.add("hide");
+
+  suggestionContainer.classList.add("hide");
+};
+
+const showWeatherData = async (city) => {
+  hideInformation();
+
+  const data = await getWeatherData(city);
+
+  if (data.cod === "404") {
+    showErrorMessage();
+    return;
+  }
+
+  cityElement.innerText = data.name;
+  tempElement.innerText = parseInt(data.main.temp);
+  descElement.innerText = data.weather[0].description;
+  weatherIconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+  );
+  countryElement.setAttribute("src", `https://flagcdn.com/16x12/${data.sys.country.toLowerCase()}.png`);
+  umidityElement.innerText = `${data.main.humidity}%`;
+  windElement.innerText = `${data.wind.speed}km/h`;
+
+  // Change bg image
+  document.body.style.backgroundImage = `url("${apiUnsplash + city}")`;
+
+  weatherContainer.classList.remove("hide");
+};
+
+searchBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  const city = cityInput.value;
+
+  showWeatherData(city);
+});
+
+cityInput.addEventListener("keyup", (e) => {
+  if (e.code === "Enter") {
+    const city = e.target.value;
+
+    showWeatherData(city);
+  }
+});
+
+// Sugestões
+suggestionButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const city = btn.getAttribute("id");
+
+    showWeatherData(city);
+  });
 });
